47c28d186dbdf83a581bdb117a5d7911226a99bc
Conrado <conradoms@gmail.com>
2020-01-28 19:57:49 -0300

    Criação do projeto backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
+const http = require('http');
+const { setupWebSocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
+
 mongoose.connect('mongodb+srv://devradar:devradar@cluster0-a5uow.mongodb.net/radar?retryWrites=true&w=majority',
 {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3330);
+server.listen(3330);
